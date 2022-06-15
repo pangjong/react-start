282e7270ed8f87408abd19c4efc4eacb7459b3ab
khs931208 <khs931208@gmail.com>
2022-06-15 21:33:31 +0900

    함수형 구구단 해보기 ( 유튜브 링크 참조 )
    https://youtu.be/BEZ4l474rxQ?list=PLcqDmjxt30RtqbStQqk-eYMK8N-1SYIFn

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [gugudan, setGugudan] = useState({
+    first: Math.ceil(Math.random() * 9),
+    second: Math.ceil(Math.random() * 9),
+    value: '',
+    result: '',
+  })
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+    
+    if(parseInt(gugudan.value) === gugudan.first * gugudan.second) {
+      setGugudan((prevState) => {
+        return {
+          ...prevState,
+          first: Math.ceil(Math.random() * 9),
+          second: Math.ceil(Math.random() * 9),
+          result: `${gugudan.value} 정답`,
+          value: '',
+        }
+      });
+    } else {
+      setGugudan((prevState) => {
+        return {
+          ...prevState,
+          result: `${gugudan.value} 오답`,
+          value: '',
+        }
+      });
+    }
+  };
+  
+  const onHandleChange = (e) => {
+    setGugudan((prevState) => {
+      return {
+        ...prevState,
+        value: e.target.value
+      }
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div> {gugudan.first} 곱하기 {gugudan.second} 는 ?</div>
+        <form onSubmit={onHandleSubmit}>
+          <input type="number" value={gugudan.value} onChange={onHandleChange} />
+          <button>입력</button>
+        </form>
+        <div> {gugudan.result}</div>
       </header>
     </div>
   );
