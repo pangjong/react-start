1c490bb5f279b173d1489c9f04c146e8bf4b0a1c
khs931208 <khs931208@gmail.com>
2022-06-15 21:53:44 +0900

    inputRef (Link)
    https://youtu.be/EUQnxfZgFJU?list=PLcqDmjxt30RtqbStQqk-eYMK8N-1SYIFn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import { useState } from 'react';
 
 function App() {
+
+  // state = {
+  //   first: Math.ceil(Math.random() * 9),
+  //   second: Math.ceil(Math.random() * 9),
+  //   value: '',
+  //   result: '',
+  // }
+
   const [gugudan, setGugudan] = useState({
     first: Math.ceil(Math.random() * 9),
     second: Math.ceil(Math.random() * 9),
     value: '',
     result: '',
   })
+  const inputRef = React.useRef();
 
   const onHandleSubmit = (e) => {
     e.preventDefault();
@@ -19,7 +29,7 @@ function App() {
           ...prevState,
           first: Math.ceil(Math.random() * 9),
           second: Math.ceil(Math.random() * 9),
-          result: `${gugudan.value} 정답`,
+          result: `${prevState.value} 정답`,
           value: '',
         }
       });
@@ -27,11 +37,13 @@ function App() {
       setGugudan((prevState) => {
         return {
           ...prevState,
-          result: `${gugudan.value} 오답`,
+          result: `${prevState.value} 오답`,
           value: '',
         }
       });
     }
+
+    inputRef.current.focus();
   };
   
   const onHandleChange = (e) => {
@@ -48,7 +60,7 @@ function App() {
       <header className="App-header">
         <div> {gugudan.first} 곱하기 {gugudan.second} 는 ?</div>
         <form onSubmit={onHandleSubmit}>
-          <input type="number" value={gugudan.value} onChange={onHandleChange} />
+          <input ref={inputRef} type="number" value={gugudan.value} onChange={onHandleChange} />
           <button>입력</button>
         </form>
         <div> {gugudan.result}</div>
