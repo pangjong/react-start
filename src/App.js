80db18721a8d3de06161f20fed2491a674119437
khs931208 <khs931208@gmail.com>
2022-06-18 21:37:07 +0900

    @help - require vs import 차이
    // import 와 require의 차이는
    // require 는 이전 Node에서 다른 객체를 불러올때 사용하는 문법 단 한줄에 하나씩만
    // import 는 es6 에서 새로 추가된 문법으로 여러개를 동시에 불러올때 사용할 수 있다.
    // webpack이 컴파일링할때 require는 모듈을 통째로 불러오는 반면에 import 는 필요한것만 불러오고 압축한다는 차이.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { useState } from 'react';
+// import 와 require의 차이는
+// require 는 이전 Node에서 다른 객체를 불러올때 사용하는 문법 단 한줄에 하나씩만
+// import 는 es6 에서 새로 추가된 문법으로 여러개를 동시에 불러올때 사용할 수 있다.
+// webpack이 컴파일링할때 require는 모듈을 통째로 불러오는 반면에 import 는 필요한것만 불러오고 압축한다는 차이.
+
 
 function App() {
 
